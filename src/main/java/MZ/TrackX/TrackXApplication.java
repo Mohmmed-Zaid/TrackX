82fac1daeecb2ce545741a2cf8daac11990c3706
Mohmmed Zaid <mohmmedzaidshaikh@gmail.com>
2025-06-15 12:26:32 +0530

    Starting with project

---
+++ src/main/java/MZ/TrackX/TrackXApplication.java
@@ -0,0 +1,13 @@
+package MZ.TrackX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackXApplication.class, args);
+	}
+
+}
